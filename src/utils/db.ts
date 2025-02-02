be72e02d7eea9831bbeea296e0f0bb85f1dd19b8
Suresh <sureshrao100000@gmail.com>
2025-02-02 19:13:27 +0530

    Remove the errors from db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,11 +3,16 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
+let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
+if (!uri) {
   throw new Error("Please add your Mongo URI to .env.local");
+}
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 if (process.env.NODE_ENV === "development") {
